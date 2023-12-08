f9fea6870754d2c0c39d93cb0ebb5efe8c5b280d
hfifre <hugo.fifre@gmail.com>
2023-12-08 12:08:11 +0100

    resolve problems in dev

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Created on Thu Dec  7 09:37:45 2023
 
 """
-import pygame, sys, random, math
+import pygame, sys, random
 from config import *
 from Drawable import *
 from Grid import *
@@ -27,7 +27,33 @@ class Game :
         self.is_playing=False
         self.to_be_started=True
         
+    def start(self):
+        playing_song = pygame.mixer_music.load("i-wanna-feel-110039.ogg")
+        pygame.mixer_music.play(-1)
+        pygame.mixer.music.set_volume(0.5)
+        SCREEN.blit(banner,(-30,0))
+        pygame.display.flip()
+        
+        while self.is_playing==False:
             
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running= False
+                    pygame.quit()
+                    sys.exit()
+            
+                if event.type == pygame.KEYDOWN:
+                    if event.key== pygame.K_SPACE :
+                        # if self.to_be_started :
+                        self.is_playing=True 
+                        print ("Le partie a commencé ! Bonne chance!")
+                        pygame.display.flip()
+                        # self.to_be_started= False
+                        pygame.mixer.music.fadeout(3)
+                        pygame.mixer_music.unload()
+                        banner_song = pygame.mixer_music.load("stranger-things-124008.ogg")
+                        pygame.mixer_music.play(-1)
+                        pygame.mixer.music.set_volume(0.3)
         
 def circle_surf(radius, color):
     surf = pygame.Surface((radius * 2, radius * 2))
@@ -36,83 +62,40 @@ def circle_surf(radius, color):
     return surf
 
 game = Game() 
+game.start()
+
+camera = Camera()
+grid = Grid(SCREEN, camera)
+miams = Miams(SCREEN, camera, NB_FOOD)
+player = Player(SCREEN, camera, "Oeuf au plat")
+painter = Painter()
+
+player_movement = False
+event_key = 0
+particles = []    
+
+painter.add(grid)
+painter.add(miams)
+painter.add(player)
+
+pygame.display.flip()
 
 running =True
 
-
-
 while running:  
-     
     clock.tick(50)
-
-    if game.is_playing==False:
-        
-        playing_song = pygame.mixer_music.load("i-wanna-feel-110039.ogg")
-        pygame.mixer_music.play(-1)
-        pygame.mixer.music.set_volume(0.3)
-        SCREEN.blit(banner,(-30,0))
-        pygame.display.flip()
-        
-        for event in pygame.event.get():
-             if event.type == pygame.QUIT:
-                 running= False
-                 pygame.quit()
-                 sys.exit()
-        
-             if event.type == pygame.KEYDOWN:
-                    if event.key== pygame.K_SPACE :
-                        if game.to_be_started :
-                                game.is_playing=True 
-                                print ("Le partie a commencé ! Bonne chance!")
-                                pygame.display.flip()
-                                game.to_be_started= False
-                                pygame.mixer.music.fadeout(3)
-                                pygame.mixer_music.unload()
-                                banner_song = pygame.mixer_music.load("stranger-things-124008.ogg")
-                                pygame.mixer_music.play(-1)
-                                pygame.mixer.music.set_volume(0.3)
-         
-                                camera = Camera()
-                                grid = Grid(SCREEN, camera)
-                                miams = Miams(SCREEN, camera, NB_FOOD)
-                                player = Player(SCREEN, camera, "Oeuf au plat")
-                                painter = Painter()
-
-                                player_movement = False
-                                event_key = 0
-                                particles = []    
-
-                                painter.add(grid)
-                                painter.add(miams)
-                                painter.add(player)
-                                
-                                
-                                
-                        else:
-                                print ("Ne quittez pas,une partie va recommencer !")
-        
-    else :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running= False
+            pygame.quit()
+            sys.exit()
     
-         for event in pygame.event.get():
-             if event.type == pygame.QUIT:
-                 running= False
-                 pygame.quit()
-                 sys.exit()
-        
-             if event.type == pygame.KEYDOWN:
-                    if event.key== pygame.K_SPACE :
-                        if game.is_playing==False and game.to_be_started :
-                                game.is_playing=True
-                                print ("Le partie a commencé ! Bonne chance!")
-                                pygame.display.flip()
-                        else:
-                                print ("Ne quittez pas,une partie va recommencer !")
-            
-                    else:
-                            player_movement = True
-                            event_key = event.key
-            if event.type == pygame.KEYUP:
-                    player_movement = False
+        if event.type == pygame.KEYDOWN:
+            player_movement = True
+            event_key = event.key
+        if event.type == pygame.KEYUP:
+            player_movement = False
+
     if player_movement:
         player.move(event.key)
 
@@ -133,14 +116,14 @@ while running:
                                         int(particle[0][1])], 
                             int(particle[2]))
 
-            radius = particle[2] * 2
-            SCREEN.blit(circle_surf(radius, (20, 20, 60)), 
-                            (int(particle[0][0] - radius), 
-                            int(particle[0][1] - radius)), 
-                            special_flags=pygame.BLEND_RGB_ADD)
+        radius = particle[2] * 2
+        SCREEN.blit(circle_surf(radius, (20, 20, 60)), 
+                        (int(particle[0][0] - radius), 
+                        int(particle[0][1] - radius)), 
+                        special_flags=pygame.BLEND_RGB_ADD)
 
-            if particle[2] <= 0:
-                particles.remove(particle)
+        if particle[2] <= 0:
+            particles.remove(particle)
 
     pygame.display.flip()
     pygame.display.update()
